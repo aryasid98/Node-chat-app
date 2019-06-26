8d4c97d2d0ce335b1fb0148761ee1e2556d8f87f
SIddharth Arya <aryasid98@gmail.com>
2019-06-26 18:32:53 +0530

    Emit to rooms

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express=require('express');
 const socketIO=require('socket.io');
 
 const {generateMessage,generateLocationMessage}=require('./utils/message');
+const {isRealString}=require('./utils/validation.js')
 const publicPath=path.join(__dirname , '../public');
 const port=process.env.PORT || 3000;
 
@@ -16,11 +17,23 @@ app.use(express.static(publicPath)); //middleware
 io.on('connection',(socket) => {
   console.log('New User Connected!!');
 
-socket.emit('newMessage', generateMessage('Admin','Welcome to the Chat App'));
+socket.on('join',(params,callback)=>{
+  if(!isRealString(params.name) || !isRealString(params.room)){
+    callback('Name and room are required.');
+  }
+  socket.join(params.room);
+  //socket.leave(params.room);
 
-socket.broadcast.emit('newMessage',generateMessage('Admin','New User Joined'));
+  //io.emit -> io.to('The Office fans').emit : This is going to send event to everyone in that Room
+  //socket.broadcast.emit -> socket.broadcast.to('The Office Fans') : Sends event to everyone except to the user sending this.
+  //socket.emit -> For particular user 4
 
-  socket.on('createMessage',(newMessage ,callback) => { // sent to user from User
+  socket.emit('newMessage', generateMessage('Admin','Welcome to the Chat App'));
+  socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} has joined.`));
+  callback();
+});
+
+socket.on('createMessage',(newMessage ,callback) => { // sent to user from User
     console.log('createMessage',newMessage);
     io.emit('newMessage',generateMessage(newMessage.from,newMessage.text));
     callback();
