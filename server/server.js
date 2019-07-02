e1fab42d7646a0c76a1fa8f04ed319c3a9113f71
SIddharth Arya <aryasid98@gmail.com>
2019-07-02 16:45:09 +0530

    Room config of Users

--- server/server.js
+++ server/server.js
@@ -39,13 +39,23 @@ socket.on('join',(params,callback)=>{
 });
 
 socket.on('createMessage',(newMessage ,callback) => { // sent to user from User
-    console.log('createMessage',newMessage);
-    io.emit('newMessage',generateMessage(newMessage.from,newMessage.text));
+    //console.log('createMessage',newMessage);
+    var user=users.getUser(socket.id);
+    if(user && isRealString(newMessage.text)){
+      //  io.emit('newMessage',generateMessage(newMessage.from,newMessage.text));
+        io.to(user.room).emit('newMessage',generateMessage(user.name,newMessage.text));
+    }
+
     callback();
   });
 
 socket.on('createLocationMessage' , (coords) =>{
-  io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
+
+  var user=users.getUser(socket.id);
+  if(user){
+    //io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
+    io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,coords.latitude,coords.longitude));
+  }
 });
 
   socket.on('disconnect', () =>{
